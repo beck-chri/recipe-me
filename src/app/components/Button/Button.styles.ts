cf385a2578cc17c07c8b6bc5edb44b4b09037b57
Marvin Laibold <marvin.laibold@aoe.com>
2023-07-09 21:02:19 +0200

    add panda css

---
+++ src/app/components/Button/Button.styles.ts
@@ -0,0 +1,16 @@
+import { css } from "../../../../styled-system/css";
+
+export const button = css({
+  backgroundColor: "rose.800",
+  borderRadius: "full",
+  fontSize: "md",
+  paddingX: "1",
+  paddingY: "2",
+  color: "white",
+  cursor: "pointer",
+  minWidth: "20",
+  boxShadow: "md",
+  _hover: {
+    backgroundColor: "rose.900",
+  },
+});
--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,7 +1,9 @@
+import { button } from "@/app/components/Button/Button.styles";
+
 type Props = {
   text: string;
 };
 
 export const Button = ({ text }: Props) => {
-  return <button>{text}</button>;
+  return <button className={button}>{text}</button>;
 };
